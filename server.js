c68c8dc5c9e4d11b77ad58441df20297e0429611
Jeremy Lincoln <bossco28@gmail.com>
2017-12-08 10:30:09 -0500

    Query Strings and Params example

--- server.js
+++ server.js
@@ -5,11 +5,56 @@ const app = express();
 
 //ROUTES
 app.get('/', (req, res)=>{
-    res.send('Welcome');
+    res.send(`
+        <html>
+            <body>
+                <h1>Welcome, buddy</h1>
+            </body>
+        </html>   
+    `);
 });
 
 
+app.get('/api/user', (req,res)=>{
+    res.send({
+        name:"Jeremy",
+        lastname: "lincoln"
+    })
+});
+
+//PARAMS
+//http://expressjs.com/en/guide/routing.html
+
+
+app.get('/api/:user/:id', (req, res)=>{
+    let id = req.params.id;
+    let user = req.params.user;
+    res.send(`
+        <html>
+            <body>
+                <p>The id is ${id} and username is ${user}</p>
+            </body>
+        </html>
+    `)
+});
+
+//query string - not built in express
+// /cars?brand=ford&year=2017
+
+//but we still can for now
+app.get("/api/car", (req, res)=>{
+    let brand = req.query.brand;
+    let year = req.query.year;
+
+    res.send({
+        brand,
+        year
+    })
+
+;})
+
 //SERVER
-app.listen(3000, ()=>{
+const port = process.env.PORT || 3000
+app.listen(port, ()=>{
     console.log('Server Running')
-})
+});
